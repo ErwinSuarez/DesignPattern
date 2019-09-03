a34eda81d8f9ef09d12c3c1687a58c022f1b0586
Erwin <erwinsuarez2003@yahoo.com>
2019-09-03 10:35:38 +0100

    created Singleton class

--- src/SingletonPattern/Singleton.java
+++ src/SingletonPattern/Singleton.java
@@ -1,4 +1,17 @@
 package SingletonPattern;
 
 public class Singleton {
+
+    private static Singleton ourInstance;
+    public static Singleton getInstance() {
+
+        if(ourInstance == null) {
+            ourInstance = new Singleton();
+        }
+        return ourInstance;
+    }
+
+    public Singleton getInfo(){
+        return ourInstance;
+    }
 }
