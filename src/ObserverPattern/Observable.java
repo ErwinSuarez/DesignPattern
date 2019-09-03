6f7c7af93aaa2992e5a066aa603654675280268e
Erwin <erwinsuarez2003@yahoo.com>
2019-09-03 11:05:23 +0100

    created update method

--- src/ObserverPattern/Observable.java
+++ src/ObserverPattern/Observable.java
@@ -1,4 +1,7 @@
 package ObserverPattern;
 
 public interface Observable {
+    void registerObservers(Observing observing);
+    void removeObservers(Observing observing);
+    void notifyObservers();
 }
