5286ec5e070fa638b5038804cd80f4f3a96641b8
Erwin <erwinsuarez2003@yahoo.com>
2019-09-03 11:29:20 +0100

    created driver class

--- src/ObserverPattern/DriverClass.java
+++ src/ObserverPattern/DriverClass.java
@@ -1,4 +1,19 @@
 package ObserverPattern;
 
 public class DriverClass {
+
+    public static void main(String[] args) {
+
+        Doctor doctor = new Doctor();
+
+        Admissions admissions = new Admissions(doctor);
+        ImagingDepartment imagingDepartment = new ImagingDepartment(doctor);
+
+        doctor.setRequest("Philip", "Chest PA", "William");
+        doctor.setRequest("John", "Ultrasound Abdomen", "Paul");
+        doctor.removeObservers(admissions);
+        doctor.setRequest("Philip", "Complete scan(DEXA)", "Fiona" );
+
+    }
+
 }
