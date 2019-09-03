f28f0bef5c72f872deeabb7a4240de11c73d5b1a
Erwin <erwinsuarez2003@yahoo.com>
2019-09-03 11:24:53 +0100

    implemented methods and created fields

--- src/ObserverPattern/ImagingDepartment.java
+++ src/ObserverPattern/ImagingDepartment.java
@@ -1,4 +1,26 @@
 package ObserverPattern;
 
-public class ImagingDepartment {
+public class ImagingDepartment implements Observing{
+
+    private String docname;
+    private String request;
+    private String patient;
+    private Observable observable;
+
+    public ImagingDepartment(Observable observable) {
+        this.observable = observable;
+        observable.registerObservers(this);
+    }
+
+    @Override
+    public void update(String docname, String request, String patient) {
+        this.docname = docname;
+        this.request = request;
+        this.patient = patient;
+        showList();
+    }
+
+    private void showList() {
+        System.out.println("IMAGING DEPT\nDr. " + docname + " resquested for a: " + request + " for Patient: " + patient);
+    }
 }
