9685f027997693b77db5ea28bdc03cd44c52da94
Erwin <erwinsuarez2003@yahoo.com>
2019-09-03 10:25:52 +0100

    created performCalculation method with method body created Calculation contructor

--- src/StrategyPattern/Calculation.java
+++ src/StrategyPattern/Calculation.java
@@ -1,5 +1,13 @@
 package StrategyPattern;
 
 public class Calculation {
+    private BasicCalculationStrategy basicCalculationStrategy;
 
+    public Calculation(BasicCalculationStrategy calculationStrategy){
+        this.basicCalculationStrategy = calculationStrategy;
+    }
+
+    public int performCalculation(int a, int b){
+        return basicCalculationStrategy.calculation(a,b);
+    }
 }
